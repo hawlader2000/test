beda9cd3605a2ef84f7a579b5408601c0379d0be
hawlader44c <mdbadolhawlader44c@gmail.com>
2022-12-05 18:33:29 +0600

    add app.js h1 tag again

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const greeting=(props)=>{
-  return <h1>WELCOME YOUR NEW PROJECT</h1>;
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const new_greeting=(props)=>{
-return (<div>
-  <h1>Hello World</h1>
-</div>)
-}
-
-const greeting=(props){
-return is_loggeedin=props.isloggeedin
-
-if(isloggeedin){
-  return <greeting />
-}
-return <new_greeting />
-}
-
-const root=ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
-<greeting is_loggeedin={false}/>
-)
+root.render(<App />);
