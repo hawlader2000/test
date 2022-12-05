4feb317e3bf9fd3ccc2291d6b6a5bf9b4f05a612
hawlader44c <mdbadolhawlader44c@gmail.com>
2022-12-05 18:47:24 +0600

    add new component HELLO

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import HEELO from "./HEELO";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Uttara UNiversity</h1>
+      <HEELO />
     </div>
   );
 }
---
+++ src/HEELO.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HEELO() {
+  return <div>HEELO</div>;
+}
