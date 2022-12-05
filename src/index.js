78d74eb6e628a6904433773a968f8dd6c4164c5c
hawlader44c <mdbadolhawlader44c@gmail.com>
2022-12-05 18:04:05 +0600

    first project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const greeting=(props)=>{
+  return <h1>WELCOME YOUR NEW PROJECT</h1>;
+}
+
+const new_greeting=(props)=>{
+return <h1>Please Sign IN</h1>;
+}
+
+const greeting=(props){
+return is_loggeedin=props.isloggeedin
+
+if(isloggeedin){
+  return <greeting />
+}
+return <new_greeting />
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<greeting is_loggeedin={false}/>
+)
